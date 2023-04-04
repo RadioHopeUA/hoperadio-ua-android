ab7e384afbfb5269affe3ad80e006d90d2a14d28
Vitalii Cherniak <cherniak.vitalii@gmail.com>
2023-04-04 10:00:10 +0300

    Fix exception on parsing stream info

--- app/src/main/kotlin/ua/hope/radio/player/StreamInfo.kt
+++ app/src/main/kotlin/ua/hope/radio/player/StreamInfo.kt
@@ -1,10 +1,17 @@
 package ua.hope.radio.player
 
+import timber.log.Timber
+
 data class StreamInfo(val title: String, val artist: String) {
     companion object {
         fun from(info: String): StreamInfo {
             val splitted = info.split(" - ")
-            return StreamInfo(splitted[1], splitted[0])
+            return if (splitted.size >= 2) {
+                StreamInfo(splitted[1], splitted[0])
+            } else {
+                Timber.e("Unable to parse stream info: $info")
+                StreamInfo(info, "")
+            }
         }
 
         val EMPTY = StreamInfo("", "")
